7dbf3a1b4e033bcf0c5ba9e3d0e893ac1930630e
Bharat Baglari <bharatbaglari@gmail.com>
2023-09-13 12:15:54 +0530

    update

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var names=new Array();
-names[0]="Yaakov";
-names[1]="John";
+var names = new Array();
+names[0]="Bharat";
+names[1]="Baglari";
 names[2]="Jen";
 names[3]="jason";
 names[4]="paul";
 names[5]="frank";
-names[6]="larry";
+names[6]="Aman";
 names[7]="paula";
 names[8]="laura";
 names[9]="jim";
